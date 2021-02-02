ce6c0d0ba2b4ace715dc39fdadac6bc8fa4140a8
Pedro Silva <kravel@outlook.com>
2021-02-02 11:33:35 -0600

    nodejs1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express= require("express")
+const app = express()
+const port = 3000;
+app.set("view engine", "ejs")
+app.set("views", __dirname+"/views")
+
+app.use(express.static(__dirname+"/public"))
+
+app.get("/", (req, res)=>{
+    res.render("index", {message:"Pagina de inicio"})
+})
+
+app.get("/servicios", (req, res)=>{
+    res.render("index", {message:"Pagina de servicios"})
+})
+
+app.use((req,res,next)=>{
+    res.render("404",{message:"Lost", descripcion:"la ruta escrita no existe"})
+})
+
+app.listen(port, ()=>{
+    console.log("Corriendo")
+})
